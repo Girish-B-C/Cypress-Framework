f2f93e3621fdfa95486140652350f5cbc875dd99
Girish <gbcr143@gmail.com>
2025-08-06 23:22:27 +0530

    FrameworkRahulbdd

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -10,16 +10,15 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+// Cypress.Commands.add("login", (email, password) => { ... })
 //
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('submitFormDetails',()=>
+{
+        cy.get("#country").type("India")
+        cy.get(".suggestions ul li a").click()
+        cy.get(".btn-success").click()
+})
+
+
+
+
