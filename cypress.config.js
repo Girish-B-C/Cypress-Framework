dcd715ca8980461b47e0b5d6b2d2cde3ac3fa351
Girish <gbcr143@gmail.com>
2025-08-10 22:17:55 +0530

    task

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,62 @@
 const { defineConfig } = require("cypress");
+const ExcelJs =require('exceljs');
+
+
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+ 
+   async writeExcelTest({searchText,replaceText,change,filePath})
+    {
+        
+      const workbook = new ExcelJs.Workbook();
+      await workbook.xlsx.readFile(filePath);
+      const worksheet = workbook.getWorksheet('Sheet1');
+      const output= await readExcel(worksheet,searchText);
+    
+      const cell = worksheet.getCell(output.row,output.column+change.colChange);
+      cell.value = replaceText;
+      //pending resolved rejected
+      return workbook.xlsx.writeFile(filePath).then(()=>
+      {
+        return true;
+      })
+      .catch((error)=>
+        {
+          return false;
+        })
+ 
+    
+    }
+ 
+  })
+ 
+ 
+ 
+ 
+async function readExcel(worksheet,searchText)
+{
+    let output = {row:-1,column:-1};
+    worksheet.eachRow((row,rowNumber) =>
+    {
+          row.eachCell((cell,colNumber) =>
+          {
+              if(cell.value === searchText)
+              {
+                  output.row=rowNumber;
+                  output.column=colNumber;
+              }
+  
+  
+          }  )
+    
+    })
+    return output;
+}
     },
+    
     specPattern: "cypress/e2e/examples/*.cy.js",
   },
 });
